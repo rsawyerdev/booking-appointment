9ce537157470ac5c570cd9ab94cdf49ed31dc467
rosemarie <rosemarie@few.io>
2025-05-11 09:57:43 -0400

    add custom google font

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,21 @@
+import {
+  Inter_400Regular,
+  Inter_900Black,
+  useFonts,
+} from '@expo-google-fonts/inter';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import Home from './home';
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    bold: Inter_900Black,
+
+    regular: Inter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Home />
