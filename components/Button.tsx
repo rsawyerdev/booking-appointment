4f5a14eff3755c7fb089c82738bfe04def548573
rosemarie <rosemarie@few.io>
2025-05-11 16:26:54 -0400

    confirmation modal styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text } from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
 
 export function Button(props: any) {
   const {
@@ -6,31 +6,38 @@ export function Button(props: any) {
     onButtonPress,
     textColor,
     backgroundColor,
+    styleProps,
   }: {
     title: string;
     onButtonPress: () => void;
     textColor: string;
     backgroundColor: string;
+    styleProps: any;
   } = props;
   return (
     <Pressable
       onPress={onButtonPress}
-      style={{
-        backgroundColor: backgroundColor,
-        borderRadius: 4,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-      }}
+      style={[
+        { ...styleProps, backgroundColor: backgroundColor },
+        styles.container,
+      ]}
     >
-      <Text style={{ color: textColor, padding: 12, fontFamily: 'regular' }}>
-        {title}
-      </Text>
+      <Text style={[styles.buttonText, { color: textColor }]}>{title}</Text>
     </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonText: { padding: 12, fontFamily: 'regular' },
+});
